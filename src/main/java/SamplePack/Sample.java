05596223c43c06e46ae53e8d182ef6503d3b59f0
Administrator <Administrator@SushmaReddy-PC.mshome.net>
2023-01-07 14:20:27 +0530

    "I am creating Sample.java file"

---
+++ src/main/java/SamplePack/Sample.java
@@ -0,0 +1,13 @@
+package SamplePack;
+
+import org.testng.annotations.Test;
+
+public class Sample {
+
+	@Test
+	public void Test1() {
+		System.out.println("This is new script for committing");
+	}
+	
+	
+}
